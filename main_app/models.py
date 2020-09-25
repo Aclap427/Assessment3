d67d4d9866767f56b5eea7eb915569718ba0f6c0
Amanda Claproth <amanda.claproth@gmail.com>
2020-09-25 12:20:43 -0700

    Complete Step 3.3

--- main_app/models.py
+++ main_app/models.py
@@ -3,7 +3,8 @@ from django.urls import reverse
 
 # Create your models here.
 class Widget(models.Model):
-    widget = models.CharField(max_length=50)
+    description = models.CharField(max_length=50)
+    quantity = models.IntegerField(default=1)
 
     def __str__(self):
         return self.widget
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from .models import *
+from .forms import *
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    widgets = Widget.objects.all()
+    widget_form = WidgetForm()
+    return render(request, 'index.html', {'widget_form' : widget_form, 'widgets' : widgets})
