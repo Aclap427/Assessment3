eef242a6a30dfe0366418f989419fb48bdb8ff63
Amanda Claproth <amanda.claproth@gmail.com>
2020-09-25 12:45:13 -0700

    Complete Step 3.4

--- main_app/models.py
+++ main_app/models.py
@@ -1,13 +1,13 @@
 from django.db import models
-from django.urls import reverse
+#from django.urls import reverse
 
 # Create your models here.
 class Widget(models.Model):
     description = models.CharField(max_length=50)
-    quantity = models.IntegerField(default=1)
+    quantity = models.IntegerField()
 
-    def __str__(self):
-        return self.widget
+    # def __str__(self):
+    #     return self.widget
 
-    def get_absolute_url(self):
-        return reverse('index')
+    # def get_absolute_url(self):
+    #     return reverse('index')
--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import *
 from .forms import *
 
 # Create your views here.
 def index(request):
     widgets = Widget.objects.all()
+    total = 0
+    for widget in widgets:
+        total += widget.quantity
     widget_form = WidgetForm()
-    return render(request, 'index.html', {'widget_form' : widget_form, 'widgets' : widgets})
+    return render(request, 'index.html', {
+        'widget_form': widget_form, 
+        'widgets': widgets,
+        'total': total
+    })
+
+def delete_widget(request, widget_id):
+    Widget.objects.filter(id=widget_id).delete()
+    return redirect('index')
+
+def add_widget(request):
+  form = WidgetForm(request.POST)
+  if form.is_valid():
+    form.save()
+  return redirect('index')
